36a2b69b24ec189a02e59862b0c3655d3c227e25
Rafael Moreira <52933778+RafaeltiMoreira@users.noreply.github.com>
2024-05-20 00:33:59 -0300

    Criação inicial do service

---
+++ src/personagem/personagem.service.js
@@ -0,0 +1,27 @@
+function readAll() {
+  return []
+}
+
+function readById() {
+
+}
+
+function create() {
+
+}
+
+function updateById() {
+
+}
+
+function deleteById() {
+
+}
+
+module.exports = {
+  readAll,
+  readById,
+  create,
+  updateById,
+  deleteById
+}
