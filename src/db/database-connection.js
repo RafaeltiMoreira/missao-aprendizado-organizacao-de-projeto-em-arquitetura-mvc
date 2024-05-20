b6d76a9407b050be15e0babcd7b8caa096993ce5
Rafael Moreira <52933778+RafaeltiMoreira@users.noreply.github.com>
2024-05-20 00:44:38 -0300

    Read All implementado no Service e Controller

--- src/db/database-connection.js
+++ src/db/database-connection.js
@@ -7,16 +7,19 @@ const keySecret = process.env.SECRET_KEY
 
 const dbUrl = `mongodb+srv://${admSecret}:${keySecret}@cluster0.3rntpik.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 const dbName = 'mongodb-arquitetura-mvc'
+const client = new MongoClient(dbUrl)
 
 async function connectToDatabase() {
-  const client = new MongoClient(dbUrl)
   console.log('Conectando ao banco de dados...')
   await client.connect()
   console.log('Banco de dados conectado com sucesso!')
+}
 
-  const db = client.db(dbName)
+function getDatabase() {
+  return client.db(dbName)
 }
 
 module.exports = {
-  connectToDatabase
+  connectToDatabase,
+  getDatabase
 }
